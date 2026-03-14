3162e8047323e519bc81cbc70c752c02a0d7a28b
ph-song <songpinhen@gmail.com>
2026-03-14 14:56:56 +0800

    chores

--- fastapi-app/app/core/settings.py
+++ fastapi-app/app/core/settings.py
@@ -3,11 +3,11 @@ from typing import Optional
 from app.core.logger import logger
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    LANGUAGE_MODEL_PROVIDER: str
+    DATABASE_URL: str = "sqlite+aiosqlite:///./app/database/database.db"
+    LANGUAGE_MODEL_PROVIDER: str = "google_genai"
+    LANGUAGE_MODEL_TYPE: str = "gemini-2.5-flash"
     LANGUAGE_MODEL_API_KEY: str
-    LANGUAGE_MODEL_TYPE: str
-    SECRET_KEY: str
+    SECRET_KEY: str = "supersecretkey"
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
